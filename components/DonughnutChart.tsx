97dc0bf9a8c4b8038053c17874fd5e9d4f126592
banti <olamuyiwavictor55@gmail.com>
2025-05-13 16:12:45 +0100

    installed doughnut chartjs

--- components/DonughnutChart.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const DonughnutChart = () => {
-  return (
-    <div>DonughnutChart</div>
-  )
-}
-
-export default DonughnutChart
--- components/TotalBalanceBox.tsx
+++ components/TotalBalanceBox.tsx
@@ -12,7 +12,7 @@ const TotalBalanceBox = ({
     
     <section className='total-balance'>
      <div className='total-balance-chart'>
-        {/*DoughnutChart*/}
+        <DoughnutChart />
 
 
     </div>
@@ -26,10 +26,10 @@ const TotalBalanceBox = ({
                 Total Current Balance
             </p>
 
-            <p className='total-balance-amount flex-center gap-2'>
+            <div className='total-balance-amount flex-center gap-2'>
                <AnimatedCounter amount={ 
                 totalCurrentBalance} />
-            </p>
+            </div>
         </div>
     </div>
     </section>
